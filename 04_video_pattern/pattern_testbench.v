95fb804d1c0049da6d7e0a30b7c54210bc14d1f7
Carlos Pantelides <cpantel@github.com>
2019-12-24 08:30:41 -0300

    added monitor for https://github.com/cpantel/VGARenderer

--- 04_video_pattern/pattern_testbench.v
+++ 04_video_pattern/pattern_testbench.v
@@ -6,12 +6,14 @@ module pattern_testbench;
   reg simul_reset;
   wire simul_hsync;
   wire simul_vsync;
+  wire [3:0]simul_RED;
   wire [3:0]simul_GREEN;
+  wire [3:0]simul_BLUE;
   
   initial begin
      $display("clock ok");
      simul_Clock = 1'b0;  
-     forever simul_Clock = #2.5 ~simul_Clock;
+     forever simul_Clock =  #1 ~simul_Clock;
   end
   
   initial begin
@@ -28,9 +30,20 @@ module pattern_testbench;
      $display("finish");
   end
   
+  /*
+always @(negedge simul_Clock)
+     $strobe("%b %b %b #%2h%2h%2h2h%2h%2h" , simul_Clock, simul_hsync, simul_vsync,
+        simul_RED, simul_RED, simul_GREEN, simul_GREEN, simul_BLUE, simul_BLUE
+     );
+     
+  
+  */
+  
    // Monitor
- //always @(*)
- //  $strobe("[@%0t] %b %b %b" , $time, {simul_reset, simul_Clock, simul_hsync}));
+ always @(negedge simul_Clock)
+   $strobe("%b %b %b #%h%h%h%h%h%h" , simul_Clock, simul_hsync, simul_vsync,
+      simul_RED, simul_RED, simul_GREEN, simul_GREEN, simul_BLUE, simul_BLUE
+   );
  
 
 pattern pattern(
@@ -38,7 +51,9 @@ pattern pattern(
   .reset(simul_reset),
   .hsync(simul_hsync),
   .vsync(simul_vsync),
-  .VGA_G(simul_GREEN)
+  .VGA_R(simul_RED),
+  .VGA_G(simul_GREEN),
+  .VGA_B(simul_BLUE)
 );
 
 
