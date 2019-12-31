a4bb1d5d703d89094aac42be54e70dc21cdb7a25
Carlos Pantelides <cpantel@github.com>
2019-12-31 09:03:23 -0300

    added sim config

--- 04_video_pattern/pattern_testbench.v
+++ 04_video_pattern/pattern_testbench.v
@@ -11,34 +11,21 @@ module pattern_testbench;
   wire [3:0]simul_BLUE;
   
   initial begin
-     $display("clock ok");
      simul_Clock = 1'b0;  
      forever simul_Clock =  #1 ~simul_Clock;
   end
   
   initial begin
-     $display("reset set");
      simul_reset = 1'b1;
      #100 simul_reset = 1'b0;
-     $display("reset unset");
   end
   
   initial begin
-     $display("run");
      repeat(40000) @(negedge simul_Clock);
      $finish;
-     $display("finish");
   end
   
-  /*
-always @(negedge simul_Clock)
-     $strobe("%b %b %b #%2h%2h%2h2h%2h%2h" , simul_Clock, simul_hsync, simul_vsync,
-        simul_RED, simul_RED, simul_GREEN, simul_GREEN, simul_BLUE, simul_BLUE
-     );
-     
-  
-  */
-  
+ 
    // Monitor
  always @(negedge simul_Clock)
    $strobe("%b %b %b #%h%h%h%h%h%h" , simul_Clock, simul_hsync, simul_vsync,
